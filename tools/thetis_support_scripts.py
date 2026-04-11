080560409a8f22f4c1d49e42e5d9210745136181
pappas-k <pappas.k@outlook.com>
2026-04-11 20:29:59 +0300

    remove dead code

--- tools/thetis_support_scripts.py
+++ tools/thetis_support_scripts.py
@@ -1,5 +1,3 @@
-import pickle
-
 from thetis import *
 from tools import utm
 
@@ -21,31 +19,3 @@ def coriolis(mesh, lat):
     coriolis_2d = Function(FunctionSpace(mesh, 'CG', 1), name="coriolis_2d")
     coriolis_2d.interpolate(f0 + beta * (x[1] - y_0))
     return coriolis_2d
-
-
-def export_final_state(inputdir, identifier, uv, elev, lagoon = None):
-    """
-    Exporting final state for subsequent simulations
-    :param inputdir: input file directory
-    :param identifier: simulation identifier
-    :param uv: velocity field
-    :param elev: elevation field
-    :param lagoon: flag if tidal range operation is included.
-    :return:
-    """
-    print_output("Exporting fields for subsequent simulation")
-
-    chk = DumbCheckpoint(inputdir + "/velocity" + str(identifier + 1), mode=FILE_CREATE)
-    chk.store(uv, name="velocity")
-    File('outputs/velocityout.pvd').write(uv)
-    chk.close()
-    chk = DumbCheckpoint(inputdir + "/elevation" + str(identifier + 1), mode=FILE_CREATE)
-    chk.store(elev, name="elevation")
-    File('outputs/elevationout.pvd').write(elev)
-    chk.close()
-
-    if lagoon is not None:
-        output_status = []
-        for i in range(len(lagoon)):
-            output_status.append(lagoon[i])
-        pickle.dump(output_status, open(inputdir+"/barrage_status_"+str(identifier+1)+".p", "wb"))
